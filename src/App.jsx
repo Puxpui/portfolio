13397651989d2cc8085c6c9a6d8dfb3a9715df31
Puxpui <pukpuidesu666@gmail.com>
2024-12-08 01:05:51 +0700

    Update : Responsive Tablet

--- src/App.jsx
+++ src/App.jsx
@@ -17,26 +17,29 @@ function App() {
       <div className='font-dcscrip bg-gradient-to-l from-[#f0f8ff] to-[#ffffff]'>
         <div className='flex justify-end'>
           <img
-            className='absolute w-[250px] rounded-full mr-[100px] mt-[50px] select-none opacity-75'
+            className='absolute md:w-[250px] w-[100px] rounded-full mr-[100px] mt-[50px] select-none opacity-75'
             src="https://i.imgur.com/t1xe1P9.png"
             alt=""
             draggable="false"
           />
         </div>
         <MyMotion direction="up" delay={0.2}>
-          <div className='ml-[100px] text-4xl font-semibold mt-[100px]'>Hello World</div>
+          <div className='ml-[100px] text-2xl md:text-4xl font-semibold mt-[100px]'>Hello World</div>
         </MyMotion>
         <MyMotion direction="up" delay={0.2}>
           <div className='flex'>
-            <div className='ml-[100px] text-4xl font-semibold'>I'am</div>
-            <div className='ml-[20px] text-4xl text-[#00B7FF] font-semibold'>Puxpui</div>
+            <div className='ml-[100px] text-2xl md:text-4xl font-semibold'>I'am</div>
+            <div className='ml-[20px] text-2xl md:text-4xl text-[#00B7FF] font-semibold'>Puxpui</div>
           </div>
         </MyMotion>
         <MyMotion direction="up" delay={0.2}>
-          <div className='ml-[100px] text-xl font-semibold text-[#666666]'>Student of Digital Business Technology</div>
+          <div className='ml-[100px] text-lg md:text-xl font-semibold text-[#666666]'>Student of Digital Business Technology</div>
         </MyMotion>
         <MyMotion direction="up" delay={0.2}>
           <div className='ml-[100px] mt-[15px] flex w-[125px] gap-2 select-none'>
+            <a href="https://discord.com/users/945701811959001139" target="_blank">
+              <img draggable="false" src="https://i.imgur.com/NTBVlLV.png" />
+            </a>
             <a href="https://github.com/Puxpui" target="_blank">
               <img draggable="false" src="https://i.imgur.com/CxlFent.png" />
             </a>
@@ -50,7 +53,7 @@ function App() {
         </MyMotion>
         <MyMotion direction="up" delay={0.2}>
           <div className='flex justify-center'>
-            <div className='mt-[20%] text-xl font-semibold text-[#666666]'>
+            <div className='mt-[20%] text-lg md:text-xl font-semibold text-[#666666]'>
               Game Developer | Front-End Developer
             </div>
           </div>
@@ -63,11 +66,11 @@ function App() {
         </MyMotion>
         <MyMotion direction="left" delay={0.1}>
           <div className='flex justify-center' id='about'>
-            <div className='mt-[10%] text-4xl font-semibold text-[#00B7FF] underline'>About Me</div>
+            <div className='mt-[10%] text-2xl md:text-4xl font-semibold text-[#00B7FF] underline'>About Me</div>
           </div>
         </MyMotion>
         <MyMotion direction="up" delay={0.2}>
-          <div className='flex justify-center text-center mt-[25px] text-xl font-semibold text-[#666666]'>
+          <div className='flex justify-center text-center mt-[25px] text-lg md:text-xl font-semibold text-[#666666]'>
             My name is Pui. I have a deep passion for computers and technology since I was young.
             <br /> with the goal of using technology to make a positive impact.
             <br /> I hope to be part of creating meaningful change for a better world.
@@ -75,46 +78,46 @@ function App() {
         </MyMotion>
         <MyMotion direction="left" delay={0.1}>
           <div className='flex justify-center'>
-            <div className='mt-[5%] text-4xl font-semibold text-[#00B7FF] underline'>Education</div>
+            <div className='mt-[5%] text-2xl md:text-4xl font-semibold text-[#00B7FF] underline'>Education</div>
           </div>
         </MyMotion>
         <MyMotion direction="up" delay={0.2}>
-          <div className='flex ml-[20%] justify-start text-start mt-[25px] text-2xl font-semibold text-[#666666]'>
+          <div className='flex ml-[20%] justify-start text-start mt-[25px] sm:text-xl text-2xl font-semibold text-[#666666]'>
             Phonphisai College of Technology
           </div>
         </MyMotion>
         <MyMotion direction="up" delay={0.2}>
           <div className='flex justify-end'>
-            <div className='text-start text-2xl mr-[25%] font-semibold text-[#666666] absolute'>2021 - Present</div>
+            <div className='text-start text-2xl mt-[10%] md:mt-0 mr-[25%] font-semibold text-[#666666] sm:text-lg absolute'>2021 - Present</div>
           </div>
         </MyMotion>
         <MyMotion direction="up" delay={0.2}>
-          <div className='flex ml-[25%] justify-start text-start mt-[px] text-xl font-semibold text-[#666666]'>Vocational Diploma (VCD)</div>
-          <div className='flex ml-[25%] justify-start text-start mt-[px] text-xl font-semibold text-[#666666]'>Vocational Certificate (VCT)</div>
+          <div className='flex ml-[25%] justify-start text-start mt-[px] text-lg md:text-xl font-semibold text-[#666666]'>Vocational Diploma (VCD)</div>
+          <div className='flex ml-[25%] justify-start text-start mt-[px] text-lg md:text-xl font-semibold text-[#666666]'>Vocational Certificate (VCT)</div>
         </MyMotion>
         <MyMotion direction="up" delay={0.2}>
-          <div className='flex ml-[20%] justify-start text-start mt-[25px] text-2xl font-semibold text-[#666666]'>Rujeejintakanon School</div>
+          <div className='flex ml-[20%] justify-start text-start mt-[10%] md:mt-[25px] sm:text-xl text-2xl font-semibold text-[#666666]'>Rujeejintakanon School</div>
         </MyMotion>
         <MyMotion direction="up" delay={0.2}>
           <div className='flex justify-end'>
-            <div className='text-start text-2xl mr-[25%] font-semibold text-[#666666] absolute'>2012 - 2021</div>
+            <div className='text-start text-2xl mr-[25%] mt-[10%] md:mt-0 font-semibold text-[#666666] sm:text-lg absolute'>2012 - 2021</div>
           </div>
         </MyMotion>
         <MyMotion direction="up" delay={0.2}>
-          <div className='flex ml-[25%] justify-start text-start mt-[px] text-xl font-semibold text-[#666666]'>Lower Secondary</div>
-          <div className='flex ml-[25%] justify-start text-start mt-[px] text-xl font-semibold text-[#666666]'>Education Primary</div>
+          <div className='flex ml-[25%] justify-start text-start mt-[px] text-lg md:text-xl font-semibold text-[#666666]'>Lower Secondary</div>
+          <div className='flex ml-[25%] justify-start text-start mt-[px] text-lg md:text-xl font-semibold text-[#666666]'>Education Primary</div>
         </MyMotion>
         {/* <MyMotion>
           <div className='flex justify-center' id='project'>
-            <div className='mt-[12%] text-4xl font-semibold'>My Project</div>
+            <div className='mt-[12%] text-2xl md:text-4xl font-semibold'>My Project</div>
           </div>
         </MyMotion>
         <MyMotion direction="up" delay={0.2}>
           <div className='flex justify-center gap-[35%]'>
-            <div className='flex text-start mt-[25px] text-xl font-semibold text-[#666666]'>
+            <div className='flex text-start mt-[25px] text-lg md:text-xl font-semibold text-[#666666]'>
               Game Project
             </div>
-            <div className='flex text-end mt-[25px] text-xl font-semibold text-[#666666]'>
+            <div className='flex text-end mt-[25px] text-lg md:text-xl font-semibold text-[#666666]'>
               Website Project
             </div>
           </div>
@@ -122,7 +125,7 @@ function App() {
         </MyMotion> */}
         <MyMotion direction="left" delay={0.1}>
           <div className='flex justify-center' id='certificate'>
-            <div className='mt-[12%] text-4xl font-semibold text-[#00B7FF] underline'>Certificate</div>
+            <div className='mt-[12%] text-2xl md:text-4xl font-semibold text-[#00B7FF] underline'>Certificate</div>
           </div>
         </MyMotion>
         <MyMotion direction="up" delay={0.2}>
@@ -148,15 +151,15 @@ function App() {
         </MyMotion>
         <MyMotion direction="left" delay={0.1}>
           <div className='flex justify-center' id='contact'>
-            <div className='mt-[12%] text-4xl font-semibold text-[#00B7FF] underline'>Contact</div>
+            <div className='mt-[12%] text-2xl md:text-4xl font-semibold text-[#00B7FF] underline'>Contact</div>
           </div>
         </MyMotion>
         <MyMotion direction="up" delay={0.2}>
-          <div className='flex justify-center text-start mt-[10px] text-xl font-semibold text-[#666666]'>
-            Email : pukpuidesu666@gmail.com
+          <div className='flex justify-center text-start mt-[10px] text-lg md:text-xl font-semibold text-[#666666]'>
+            <a href="mailto:pukpuidesu666@gmail.com">Email : pukpuidesu666@gmail.com</a>
           </div>
-          <div className='flex justify-center text-start mt-[px] text-xl font-semibold text-[#666666]'>Discord : Puxpui</div>
-          <div className='flex justify-center text-start mt-[px] text-xl font-semibold text-[#666666]'>GitHub : Puxpui</div>
+          <div className='flex justify-center text-start mt-[px] text-lg md:text-xl font-semibold text-[#666666]'><a href="https://discord.com/users/945701811959001139">Discord : Puxpui</a></div>
+          <div className='flex justify-center text-start mt-[px] text-lg md:text-xl font-semibold text-[#666666]'> <a href="https://github.com/Puxpui">GitHub : Puxpui</a></div>
         </MyMotion>
         <MyMotion direction="up" delay={0.2}>
           <div className='flex justify-center mt-[7.5%]'>
--- src/component/MyNavbar.jsx
+++ src/component/MyNavbar.jsx
@@ -1,4 +1,12 @@
+import { div } from "framer-motion/client";
+import { useState } from "react";
+
 function MyNavbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen)
+  }
+
   const scrollToSection = (id) => {
     document.getElementById(id).scrollIntoView({
       behavior: 'smooth',
@@ -9,31 +17,72 @@ function MyNavbar() {
   return (
     <div className="text-black font-dcscrip sticky top-0 z-10 bg-white select-none">
       <div className="flex absolute p-[12px] ml-2 text-[25px] font-semibold" onClick={() => scrollToSection('home')}>Puxpui</div>
-      <div className="flex justify-end p-4 gap-8 text-[20px] font-semibold">
-        <div className="relative group" onClick={() => scrollToSection('home')}>
-          <div className="relative">Home</div>
-          <div className="absolute left-0 bottom-0 h-[2px] w-0 bg-[#00B7FF] rounded-full transition-all duration-300 group-hover:w-full"></div>
+      <div className="flex justify-end">
+        <div className="md:hidden m-4 absolute" onClick={toggleMenu}>
+          <button id="menu-toggle" className="text-black">
+            <svg fill="none"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              viewBox="0 0 24 24"
+              className="w-6 h-6"
+            >
+              <path d='M4 6h16M4 12h16M4 18h16'></path>
+            </svg>
+          </button>
         </div>
-        <div className="relative group" onClick={() => scrollToSection('about')}>
-          <div className="relative">About</div>
-          <div className="absolute left-0 bottom-0 h-[2px] w-0 bg-[#00B7FF] rounded-full transition-all duration-300 group-hover:w-full"></div>
-        </div>
-        {/* กำลังพัฒนา
+      </div>
+
+      {/* Mobile */}
+      {isMenuOpen ? (
+            <div className="flex justify-end">
+              <div className="md:hidden flex-col mt-[45px] ml-20 p-4 gap-8 text-[20px] font-semibold">
+                <div className="relative group" onClick={() => scrollToSection('home')}>
+                  <div className="relative">Home</div>
+                </div>
+                <div className="relative group" onClick={() => scrollToSection('about')}>
+                  <div className="relative">About</div>
+                </div>
+                {/* กำลังพัฒนา
+        <div className="relative group" onClick={() => scrollToSection('project')}>
+          <div className="relative">Project</div> 
+        </div> */}
+                <div className="relative group" onClick={() => scrollToSection('certificate')}>
+                  <div className="relative">Certificate</div>
+                </div>
+                <div className="relative group" onClick={() => scrollToSection('contact')}>
+                  <div className="relative">Contact</div>
+                </div>
+              </div>
+            </div>
+      ) : null}
+
+            <div className="hidden md:flex justify-end p-4 gap-8 text-[20px] font-semibold">
+              <div className="relative group" onClick={() => scrollToSection('home')}>
+                <div className="relative">Home</div>
+                <div className="absolute left-0 bottom-0 h-[2px] w-0 bg-[#00B7FF] rounded-full transition-all duration-300 group-hover:w-full"></div>
+              </div>
+              <div className="relative group" onClick={() => scrollToSection('about')}>
+                <div className="relative">About</div>
+                <div className="absolute left-0 bottom-0 h-[2px] w-0 bg-[#00B7FF] rounded-full transition-all duration-300 group-hover:w-full"></div>
+              </div>
+              {/* กำลังพัฒนา
         <div className="relative group" onClick={() => scrollToSection('project')}>
           <div className="relative">Project</div> 
           <div className="absolute left-0 bottom-0 h-[2px] w-0 bg-[#00B7FF] rounded-full transition-all duration-300 group-hover:w-full"></div>
         </div> */}
-        <div className="relative group" onClick={() => scrollToSection('certificate')}>
-          <div className="relative">Certificate</div>
-          <div className="absolute left-0 bottom-0 h-[2px] w-0 bg-[#00B7FF] rounded-full transition-all duration-300 group-hover:w-full"></div>
-        </div>
-        <div className="relative group" onClick={() => scrollToSection('contact')}>
-          <div className="relative">Contact</div>
-          <div className="absolute left-0 bottom-0 h-[2px] w-0 bg-[#00B7FF] rounded-full transition-all duration-300 group-hover:w-full"></div>
-        </div>
-      </div>
-    </div>
-  );
+              <div className="relative group" onClick={() => scrollToSection('certificate')}>
+                <div className="relative">Certificate</div>
+                <div className="absolute left-0 bottom-0 h-[2px] w-0 bg-[#00B7FF] rounded-full transition-all duration-300 group-hover:w-full"></div>
+              </div>
+              <div className="relative group" onClick={() => scrollToSection('contact')}>
+                <div className="relative">Contact</div>
+                <div className="absolute left-0 bottom-0 h-[2px] w-0 bg-[#00B7FF] rounded-full transition-all duration-300 group-hover:w-full"></div>
+              </div>
+            </div>
+          </div>
+          );
 }
 
-export default MyNavbar;
+          export default MyNavbar;
